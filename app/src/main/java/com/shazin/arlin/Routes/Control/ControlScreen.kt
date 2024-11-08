e2d4c7fe557b11a4f8ef3705f603becccb7318b6
Shazin <shazin.bd@gmail.com>
2024-11-08 09:22:17 +0600

    Added app/src/main/java/com/shazin/arlin/Routes/Control/ControlScreen.kt;

---
+++ app/src/main/java/com/shazin/arlin/Routes/Control/ControlScreen.kt
@@ -0,0 +1,11 @@
+package com.shazin.arlin.Routes.Control
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.shazin.arlin.Models.RouteProps
+
+@Composable
+fun ControlScreen(routeProps: RouteProps){
+    Text(text = "Hello from control hub")
+    
+}
